821d5060ceec633073d12873b68cf9b1172064d2
David Fernandes <daviidmatosfernandes@gmail.com>
2025-12-23 15:18:26 +0000

    hardware-config add

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
-	home-manager/nixos
+      /hardware-configuration.nix
+	# home-manager/nixos
     ];
 
   # Bootloader.
