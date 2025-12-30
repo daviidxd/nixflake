3a7dba4817efee1381032c2c5bda71029873a002
David Fernandes <daviidmatosfernandes@gmail.com>
2025-12-30 12:30:10 +0000

    new keybinds for ghostty

--- programs/ghostty/ghostty.nix
+++ programs/ghostty/ghostty.nix
@@ -5,5 +5,20 @@
         theme = "Embers Dark";
         command = "zsh";
         font-family = "Meslo";
+      
+        # Keybind Config
+        keybind = [
+        "ctrl+r=reload_config"
+        "ctrl+n=new_tab"
+        "ctrl+a=new_split:left"
+        "ctrl+d=new_split:right"
+        
+        "ctrl+,=resize_split:left,10"
+        "ctrl+.=resize_split:right,10"
+
+        # Tab Navigation
+        "alt+1=previous_tab"
+        "alt+2=next_tab"
+        ];
     };
 }
